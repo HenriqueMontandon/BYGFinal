fbf8e4a4100f65bc6a1f800d6c75ef83d90dba51
Henrique Detal <henrique.silveira1802@usp.br>
2023-11-26 20:01:43 -0300

    ajustando models

--- destinos/models.py
+++ destinos/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.db import models
 
+class Categoria(models.Modek):
+    name = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField(auto_now_add=True)
 
 class Empresa(models.Model):
     razao_social = models.CharField(max_length=255)
@@ -15,26 +18,48 @@ class Empresa(models.Model):
 
     def _str_(self):
         return f'{self.nome_fantasia}'
-
+    
 class Destino(models.Model):
     name = models.CharField(max_length=255)
-    categoria = models.CharField(max_length=255)
     destino_url = models.URLField(max_length=200, null=True)
     descricao = models.CharField(max_length=1000)
-    likes = models.IntegerField()
+    coordenadas = models.CharField(max_length = 100)
+    preco = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    empresaid=models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'{self.name} - {self.likes}'
+    def _str_(self):
+        return f'{self.name}'
+
     
 
 class List(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    Nome = models.CharField(max_length=255)
     Capa = models.URLField(max_length=5000, null=True)
-    atracoes = models.ManyToManyField(Destino)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_alteracao = models.DateTimeField(auto_now= True)
+    duracao = models.IntegerField()
+    nome = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=1000)
+    comentario = models.CharField(max_length=500)
+    nota = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.Nome} by {self.autor.username}'
+    def _str_(self):
+        return f'Roteiro by {self.autor.username}'
+
+class Evento(models.Model):
+    destino_id = models.ForeignKey(Destino, on_delete=models.CASCADE)
+    roteiro_id = models.ForeignKey(List, on_delete=models.CASCADE)
+    data_inicio = models.DateTimeField()
+    data_fim = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_delecao = models.DateTimeField(null = True)
+    comentario = models.CharField(max_length=500, null = True)
+    nota = models.FloatField(null = True)
+
+    def _str_(self):
+        return f'{self.destino_id.nome} de {self.data_inicio} a {self.data_fim}'
     
 class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
