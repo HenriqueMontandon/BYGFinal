dd2f1292fa470e06f9466a9826717b5d40e9965f
Victor Hugo Ribeiro Leite da Silva <victorhrls@usp.br>
2023-11-26 11:53:44 -0300

    Colocando email

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm  # Importe o novo formulário
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('index'))
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     context = {'form': form}
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/signup.html',context)
