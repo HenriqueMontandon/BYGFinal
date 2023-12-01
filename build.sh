8f0051eb3f2a6a6745e1e18e65a40be4dcfd1a4d
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-30 22:42:00 -0300

    Build do Render [3]

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# adicione linhas abaixo
+
+python manage.py makemigrations
 python manage.py migrate
 
-# create superuser if missing
 cat <<EOF | python manage.py shell
 import os
 from django.contrib.auth import get_user_model
@@ -14,10 +14,16 @@ from django.contrib.auth.models import Group, Permission
 
 User = get_user_model()
 
-User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 
-g = Group(name="empresas_users")
-g.permissions.set([Permission.objects.get(codename=c) for c in ["view_categoria", "view_review", "view_categorie", "view_comment"]])
-Group.objects.filter(name="empresas_users").exists() or g.save()
+empresas_users_group, _ = Group.objects.get_or_create(name="empresas_users")
+
+permissions = ["view_categoria, view_review, view_categorie"]
+permissions_objs = [Permission.objects.get(codename=c) for c in permissions]
+
+for perm in permissions_objs:
+    empresas_users_group.permissions.add(perm)
+
+empresas_users_group.save()
 EOF
