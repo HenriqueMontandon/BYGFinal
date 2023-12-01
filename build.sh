8733077a79a58d180e2e173ec521cf28a98e75b1
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-30 22:23:49 -0300

    Build do Render [1]

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 # adicione linhas abaixo
-
 python manage.py migrate
-
-gunicorn BYGsite.wsgi:application --bind 0.0.0.0:$PORT
 
 # create superuser if missing
 cat <<EOF | python manage.py shell
 import os
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
 User = get_user_model()
 
 User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+
+g = Group(name="empresas_users")
+g.permissions.set([Permission.objects.get(codename=c) for c in ["view_categoria", "view_review", "view_categorie", "view_comment"]])
+Group.objects.filter(name="empresas_users").exists() or g.save()
 EOF
