eb5798615f36824257d8345bf9197f0656d5211e
HenriqueMontandon <91027812+HenriqueMontandon@users.noreply.github.com>
2023-12-06 14:41:59 -0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def signup(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            send_welcome_email(user.email)  # Envie o e-mail de boas-vindas
+            #send_welcome_email(user.email)  # Envie o e-mail de boas-vindas
             return HttpResponseRedirect(reverse('index'))
     else:
         form = CustomUserCreationForm()
@@ -20,15 +20,15 @@ def signup(request):
     context = {'form': form}
     return render(request, 'accounts/signup.html', context)
 
-def send_welcome_email(user_email):
-    subject = 'Bem-vindo à BYG!'
-    message = (
-        'Parabéns, você se cadastrou no nosso site. '
-        'Agora você vai poder desfrutar do melhor que a BYG pode oferecer. '
-        'Venha viajar conosco.'
-    )
-    from_email = 'bygsiteoficial@outlook.com'  # Seu endereço de e-mail
-    send_mail(subject, message, from_email,[user_email])
+#def send_welcome_email(user_email):
+ #   subject = 'Bem-vindo à BYG!'
+  #  message = (
+   #     'Parabéns, você se cadastrou no nosso site. '
+    #    'Agora você vai poder desfrutar do melhor que a BYG pode oferecer. '
+     #   'Venha viajar conosco.'
+    #)
+    #from_email = 'bygsiteoficial@outlook.com'  # Seu endereço de e-mail
+    #send_mail(subject, message, from_email,[user_email])
 
 def empresa_signup(request):
     if request.method == 'POST':
